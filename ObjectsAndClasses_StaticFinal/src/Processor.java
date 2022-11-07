eadaf56bc5c50d0b1db723cc75f86acbbc7863ad
Gorensky Aleksey <Lexagorenski@yandex.ru>
2022-11-07 09:54:58 +0700

    Предварительно 6,8 задание 2

--- ObjectsAndClasses_StaticFinal/src/Processor.java
+++ ObjectsAndClasses_StaticFinal/src/Processor.java
@@ -1,7 +1,7 @@
 public class Processor {
     public  int frequency = 500; //гц
        final int numberOfCores=2;
-       final double weight=200;
+       final double weight=205;
 
        public double getWeight() {
         return weight;
