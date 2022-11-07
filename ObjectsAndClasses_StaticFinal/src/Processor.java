6fc29c35b626bbef9f4ba2817f9e036c68760136
Gorensky Aleksey <Lexagorenski@yandex.ru>
2022-11-07 09:59:17 +0700

    Revert "Предварительно 6,8 задание 2"
    This reverts commit eadaf56b

--- ObjectsAndClasses_StaticFinal/src/Processor.java
+++ ObjectsAndClasses_StaticFinal/src/Processor.java
@@ -1,7 +1,7 @@
 public class Processor {
     public  int frequency = 500; //гц
        final int numberOfCores=2;
-       final double weight=205;
+       final double weight=200;
 
        public double getWeight() {
         return weight;
