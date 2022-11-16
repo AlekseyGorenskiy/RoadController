9d5f479f58e2952be2c83fed4dc3d8fff175dafd
Gorensky Aleksey <Lexagorenski@yandex.ru>
2022-11-16 22:46:39 +0700

    Загружаем 6,8 задание 16.11

---
+++ ObjectsAndClasses_StaticFinal/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main (String[] args){
+
+        Processor processor1=new Processor(500,200,500);
+        StorageOfInformation storageOfInformation1=new StorageOfInformation("SSD",200,200);
+        Keyboard keyboard1=new Keyboard("ппп",23,"22");
+        Display display1=new Display(32,"HDD",25);
+        Ram ram1=new Ram("DDR",256,200);
+
+        Computer computer1=new Computer("LG","DNS",processor1,storageOfInformation1,keyboard1,ram1,display1);
+        computer1.getTotalWeight();
+        System.out.println(computer1);
+
+    }
+}
