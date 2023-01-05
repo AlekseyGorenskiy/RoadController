8030fedde80c11fb2a8a20f73c39245b77532142
Gorensky Aleksey <Lexagorenski@yandex.ru>
2023-01-05 22:37:23 +0700

    7.9 Практическая работа загружаем

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -3,8 +3,15 @@ public class Main {
         Container container = new Container();
         container.addCount(5672);
         System.out.println(container.getCount());
+        for (int i=1040; i < 1104;i++) {
+            char c = (char) i;
+            System.out.println(i +"-"+ c);
+        }
+        int i = 1105;
+        char c =(char)i;
+        System.out.println(i+"-"+c);
 
-        // TODO: ниже напишите код для выполнения задания:
+        // TODO: char  ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
 
